9aea35c22b47085e26e5ca87d26f836c772a39a6
Adrian Schneider <adrian.schneider.pr@gmail.com>
2024-08-18 23:18:12 +0200

    Finnished solver

--- soldoku.py
+++ soldoku.py
@@ -1,9 +1,11 @@
+import time
+
 class Soldoku:
 
     full_set = {1, 2, 3, 4, 5, 6, 7, 8, 9}
 
     def __init__(self, field_str: str):
-        self.grid = [[self.full_set for _ in range(9)] for _ in range(9)]
+        self.grid = [[self.full_set.copy() for _ in range(9)] for _ in range(9)]
         rows = field_str.split(';')
         assert len(rows) == 9
         for r_idx, row in enumerate(rows):
@@ -17,9 +19,36 @@ class Soldoku:
         for row in self.grid:
             print(" | ".join("{" + ",".join(map(str, sorted(cell))) + "}" for cell in row))
 
+    def reduce_all(self):
+        for row_idx in range(9):
+            for col_idx in range(9):
+                self.reduce(row_idx, col_idx)
+
     def reduce(self, row_idx:int, col_idx:int):
         assert 0 <= row_idx < 9
         assert 0 <= col_idx < 9
+
+        if len(self.grid[row_idx][col_idx]) == 1:
+            return # nothing to reduce
+
+        for neighbour_sets in [self.get_neighbour_block_sets(row_idx, col_idx), self.get_neighbour_vertical_sets(row_idx, col_idx), self.get_neighbour_horizontal_sets(row_idx, col_idx)]:
+
+            # if there is a fix value in neighbour set, then remove it from mine
+            for s in neighbour_sets:
+                if len(s) == 1:
+                    self.grid[row_idx][col_idx] -= s
+
+            # if I have a value in my set, which no neighbour set has, then I am this value
+            for my_option in self.grid[row_idx][col_idx]:
+                found_second_option = False
+                for s in neighbour_sets:
+                    if my_option in s:
+                        found_second_option = True
+                        break
+
+                if not found_second_option:
+                    self.grid[row_idx][col_idx] = {my_option}
+
 
     def get_neighbour_horizontal_sets(self, row_idx:int, col_idx:int):
         assert 0 <= row_idx < 9
@@ -43,8 +72,31 @@ class Soldoku:
 
         return sets
 
+    def get_neighbour_block_sets(self, row_idx:int, col_idx:int):
+        assert 0 <= row_idx < 9
+        assert 0 <= col_idx < 9
+        sets = []
 
+        row_block_idx = row_idx // 3
+        col_block_idx = col_idx // 3
 
+        for ridx in range(3):
+            row_idx_norm = row_block_idx * 3 + ridx
+            for cidx in range(3):
+                col_idx_norm = col_block_idx * 3 + cidx
+                if (row_idx_norm != row_idx) or (col_idx_norm != col_idx):
+                    sets.append(self.grid[row_idx_norm][col_idx_norm])
+
+        return sets
+
+    def get_total_number_of_set_values(self):
+        """As lower this number, as more solved. Lowest value is 81."""
+        cnt = 0
+        for row in self.grid:
+            for cell in row:
+                cnt += len(cell)
+
+        return cnt
 
 # Example usage:
 # sudoku = Soldoku("9,8,-,4,-,-,-,-,1;-,7,-,8,-,3,-,-,6;-,-,1,-,-,9,-,5,-;-,4,-,-,2,-,5,-,-;-,5,-,3,-,8,-,4,-;1,-,6,-,7,-,-,9,-;-,-,4,5,-,-,1,-,-;3,-,-,6,-,2,-,8,-;8,-,-,-,-,1,-,7,4")
--- test_soldoku.py
+++ test_soldoku.py
@@ -1,4 +1,6 @@
 import unittest
+import time
+
 from soldoku import Soldoku
 
 class Soldoku_Tests(unittest.TestCase):
@@ -37,6 +39,70 @@ class Soldoku_Tests(unittest.TestCase):
         self.assertEqual(sets[1], {6})
         self.assertEqual(sets[2], {9})
         self.assertEqual(sets[3], dok.full_set)
+
+    def test_getblock(self):
+        dok = Soldoku(self.test_game)
+
+        sets = dok.get_neighbour_block_sets(0, 0)
+        self.assertEqual(len(sets), 8)
+        self.assertEqual(sets[0], {8})
+        self.assertEqual(sets[1], {4})
+        self.assertEqual(sets[2], dok.full_set)
+        self.assertEqual(sets[3], dok.full_set)
+
+        sets = dok.get_neighbour_block_sets(2, 0)
+        self.assertEqual(len(sets), 8)
+        self.assertEqual(sets[0], {9})
+        self.assertEqual(sets[1], {8})
+        self.assertEqual(sets[2], {4})
+        self.assertEqual(sets[3], dok.full_set)
+
+        sets = dok.get_neighbour_block_sets(3, 7)
+        self.assertEqual(len(sets), 8)
+        self.assertEqual(sets[0], {3})
+        self.assertEqual(sets[1], {4})
+        self.assertEqual(sets[2], dok.full_set)
+        self.assertEqual(sets[3], dok.full_set)
+
+        sets = dok.get_neighbour_block_sets(5, 7)
+        self.assertEqual(len(sets), 8)
+        self.assertEqual(sets[0], {3})
+        self.assertEqual(sets[1], dok.full_set)
+        self.assertEqual(sets[2], {4})
+        self.assertEqual(sets[3], dok.full_set)
+
+
+    def test_reduce(self):
+        dok = Soldoku(self.test_game)
+        dok.reduce(1, 0)
+
+        self.assertEqual(dok.grid[1][0], {1, 5, 7})
+
+    def test_num_set_vals(self):
+        dok = Soldoku(self.test_game)
+
+        should_be = 9 * 9 * 9 - (8 * 29)
+        is_now = dok.get_total_number_of_set_values()
+
+        self.assertEqual(should_be, is_now)
+
+    def test_reduce_all(self):
+        dok = Soldoku(self.test_game)
+        print("Reducing:")
+        num_set_vals_before = dok.get_total_number_of_set_values()
+        for _ in range(6):
+            dok.reduce_all()
+            num_set_vals_after = dok.get_total_number_of_set_values()
+            print("From ", num_set_vals_before, " to ", num_set_vals_after)
+            num_set_vals_before = num_set_vals_after
+
+        dok.display()
+
+        #self.assertLess(num_set_vals_after, num_set_vals_before)
+
+if __name__ == "__main__":
+    test = Soldoku_Tests()
+    test.test_reduce_all()
 
 
 
